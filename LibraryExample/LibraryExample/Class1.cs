b59491f3c2e224d2764aef35a6bd93d97a5be410
felipermartins <felipe.rmartins@outlook.com>
2014-10-31 16:05:19 -0300

    Criei biblioteca classe1
    Blah Blah Blah!

---
+++ LibraryExample/LibraryExample/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryExample
+{
+    public class Class1
+    {
+        public Class1()
+        {
+
+        }
+
+        public void doTestMessage()
+        {
+            Console.WriteLine("Yeap!");
+        }
+    }
+}
